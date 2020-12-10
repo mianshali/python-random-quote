95823e92e04f1fc14e0382b278dae5f0cffbba14
mianshali <75761079+mianshali@users.noreply.github.com>
2020-12-10 07:45:00 +0500

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import requests
+
+def get_random_quote():
+    """Get a random quote."""
+
+    res = requests.get(_QUOTE_URL)
+    return res.json()['contents']['quotes'][0]['quote']
+
+def display_quote():
+    """Display a random quote."""
+
+    print(f'My random quote is: "{get_random_quote()}"')
+
+if __name__ == '__main__':
+    display_quote()
