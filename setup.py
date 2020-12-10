6c969be09d432e2cfed7395ba78943874f2d2c8b
mianshali <75761079+mianshali@users.noreply.github.com>
2020-12-10 07:45:59 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='randomquote',
+    version='0.1',
+    description='Get a random quote',
+    url='https://github.com/mianshali/python-random-quote.git',
+    author='mian ali',
+    author_email='mianshali5@gmail.com',
+    license='MIT',
+    install_requires=['requests'],
+    packages=find_packages(),
+    entry_points=dict(
+         console_scripts=['rq=src.main:display_quote']
+    )
+)
